3f616ca41ab4c5a201bbe09709e88b5d5e5acb98
Noufia2023 <noufinaz@gmail.com>
2024-05-06 16:10:57 +0530

    feature-homepage

--- userauth/settings.py
+++ userauth/settings.py
@@ -81,21 +81,21 @@ WSGI_APPLICATION = 'userauth.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
-import dj_database_url
 
-DATABASES = {
+#import dj_database_url
 
- 'default': dj_database_url.parse(env('DATABASE_URL'))
+#DATABASES = {
 
-}
+# 'default': dj_database_url.parse(env('DATABASE_URL'))
+
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
